0cfc0de99cb99ad71c0afe220e20e51bf85b3fd8
TPride <tranielmp8@gmail.com>
2024-02-09 17:05:28 -0600

    adding another table

---
+++ prisma/migrations/20240209230208_posts/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "posts" (
+    "post_id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("post_id")
+);
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -2,34 +2,65 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 // top level await
-const user = await prisma.user.findFirst();
+
+// const users = await prisma.user.findMany({orderBy: {name: 'asc'}});
+// const users = await prisma.user.findMany({where: {email: {endsWith: 'gmail.com'}}});
+
+const user = await prisma.user.update({
+  where: {id: 5},
+  data: {name: 'BigBowser'}
+})
+
 console.log(user)
 
-// await prisma.user.deleteMany();
+// const users = await prisma.user.createMany({
+//   data: [
+//     {
+//       email: "mario@gmail.com",
+//       name: "Super Mario",
+//       username: "smario",
+//     },
+//     {
+//       email: "luigi@gmail.com",
+//       name: "Luigi",
+//       username: "luigi123",
+//     },
+//     {
+//       email: "peach@gmail.com",
+//       name: "Princess Peach",
+//       username: "peachy",
+//     },
+//     {
+//       email: "bowser@gmail.com",
+//       name: "Bowser",
+//       username: "kingkoopa",
+//     },
+//     {
+//       email: "toad@gmail.com",
+//       name: "Toad",
+//       username: "toadstool",
+//     },
+//     {
+//       email: "yoshi@gmail.com",
+//       name: "Yoshi",
+//       username: "yoshifan",
+//     },
+//     {
+//       email: "donkeykong@gmail.com",
+//       name: "Donkey Kong",
+//       username: "dkong",
+//     },
+//     {
+//       email: "wario@gmail.com",
+//       name: "Wario",
+//       username: "Wbadmario",
+//     },
+//     {
+//       email: "waluigi@gmail.com",
+//       name: "Waluigi",
+//       username: "waluigism",
+//     },
+//   ],
+// });
 
-
-// async function main() {
-//   const user = await prisma.user.findFirst();
-//   console.log(user);
-
-//   // const user = await prisma.user.create({
-//   //   data: {
-//   //     email: 'super@gmail.com',
-//   //     name: 'Super Mario',
-//   //     username: 'SMario'
-//   //   },
-//   // })
-
-//   console.log(user);
-// }
-
-// main() 
-//   .then(async () => {
-//     await prisma.$disconnect();
-
-//   })
-//   .catch(async (e) => {
-//     console.log(e)
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   })
+// console.log(users)
